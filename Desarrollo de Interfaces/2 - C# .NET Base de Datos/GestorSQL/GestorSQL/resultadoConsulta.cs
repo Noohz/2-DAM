a74173edb5c08a7510861ded823e7856266d4701
Aitor Ramos <aitorramossanchez2001@gmail.com>
2023-11-30 20:27:24 +0100

    Add files via upload

---
+++ Desarrollo de Interfaces/2 - C# .NET Base de Datos/GestorSQL/GestorSQL/resultadoConsulta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestorSQL
+{
+    public partial class resultadoConsulta : Form
+    {
+        public resultadoConsulta()
+        {
+            InitializeComponent();
+        }
+
+        public void SetResultado(string resultado)
+        {
+            textBoxConsultaR1.Text = resultado;
+        }
+    }
+}
