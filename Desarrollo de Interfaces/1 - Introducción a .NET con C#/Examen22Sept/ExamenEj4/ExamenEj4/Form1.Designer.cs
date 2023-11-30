8f5ec39c5996a8782408fc57f4b555455a9f97fd
Aitor Ramos <aitorramossanchez2001@gmail.com>
2023-11-30 14:11:13 +0100

    Add files via upload

---
+++ Desarrollo de Interfaces/1 - Introducción a .NET con C#/Examen22Sept/ExamenEj4/ExamenEj4/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ExamenEj4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblPuntuacion = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblPuntuacion
+            // 
+            this.lblPuntuacion.AutoSize = true;
+            this.lblPuntuacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntuacion.Location = new System.Drawing.Point(12, 9);
+            this.lblPuntuacion.Name = "lblPuntuacion";
+            this.lblPuntuacion.Size = new System.Drawing.Size(24, 25);
+            this.lblPuntuacion.TabIndex = 1;
+            this.lblPuntuacion.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPuntuacion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblPuntuacion;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Desarrollo de Interfaces/1 - Introducción a .NET con C#/Examen22Sept/ExamenEj4/ExamenEj4/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ExamenEj4
+{
+    public partial class Form1 : Form
+    {
+
+        Random aleatorio = new Random();
+        int puntuacion = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            MessageBox.Show("Aparecerán labels aleatorios por el formulario, si clickas en ellos ganaras un punto, si fallas perderas 1.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int x = aleatorio.Next(400);
+            int y = aleatorio.Next(400);
+
+            Label lb = new Label();
+            lb.Height = 20;
+            lb.Width = 50;
+            lb.Location = new Point(x, y);
+            lb.BackColor = Color.Red;
+            lb.Click += Lb_Click;
+
+
+            this.Controls.Add(lb);
+        }
+
+        private void Lb_Click(object sender, EventArgs e)
+        {
+
+            Label lb = (Label)sender;
+
+            puntuacion++;
+
+            lblPuntuacion.Text = puntuacion.ToString();
+
+            lb.Visible = false;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            puntuacion--;
+
+            lblPuntuacion.Text = puntuacion.ToString();
+        }
+    }
+}
