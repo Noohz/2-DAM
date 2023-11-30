aaababe35fd794841cce84b5acbab1b69a0a05c6
Aitor Ramos <aitorramossanchez2001@gmail.com>
2023-11-30 21:14:28 +0100

    Add files via upload

--- Desarrollo de Interfaces/3 - JavaScript/P2_JS_24N/js/script.js
+++ Desarrollo de Interfaces/3 - JavaScript/P2_JS_24N/js/script.js
@@ -1,27 +1,105 @@
-const cuerpo = document.body;
+window.onload = inicio;
 
-let contenedorPrincipal = document.createElement("div");
-contenedorPrincipal.className = "container";
+const componentes = [];
+var contenedorAzul;
+var contenedorVerde;
+var contenedorRojo;
+function inicio() {
 
-let contenedor = document.createElement("fieldset");
-contenedor.className = "gallery";
+    let boton = document.getElementById("mover");
+    boton.onclick = mover;
+    let contenedorBody = document.querySelector("body");
+    let contenedorPrincipal = document.createElement("div");
+    contenedorPrincipal.className = "container";
 
-let legenda = document.createElement("legend");
-legenda.textContent = "Total";
-contenedor.appendChild(legenda);
+    //contenedores secundarios
+    let contenedorTodos = document.createElement("fieldset");
+    contenedorTodos.className = "gallery";
+    let leyendaTodos = document.createElement("legend");
+    leyendaTodos.textContent = "Total";
+    contenedorTodos.appendChild(leyendaTodos);
 
-var Colores = ["Azul", "Rojo", "Naranja", "Verde"];
-var idColores = ["blue", "red", "orange", "green"];
+    //contenedor azul
+    contenedorAzul = document.createElement("fieldset");
+    contenedorAzul.className = "gallery";
+    let leyendaAzul = document.createElement("legend");
+    leyendaAzul.textContent = "Azul";
+    contenedorAzul.appendChild(leyendaAzul);
 
-for (let i = 0; i < 20; i++) {
-    let numAl = Math.floor(Math.random() * Colores.length);
+    //contenedores verde
+    contenedorVerde = document.createElement("fieldset");
+    contenedorVerde.className = "gallery";
+    let leyendaVerde = document.createElement("legend");
+    leyendaVerde.textContent = "Verde";
+    contenedorVerde.appendChild(leyendaVerde);
 
-    let boton = document.createElement("button");
-    boton.textContent = Colores[numAl];
-    boton.style.backgroundColor = idColores[numAl];
+    //contenedores Rojo
+    contenedorRojo = document.createElement("fieldset");
+    contenedorRojo.className = "gallery";
+    let leyendaRojo = document.createElement("legend");
+    leyendaRojo.textContent = "Rojo";
+    contenedorRojo.appendChild(leyendaRojo);
 
-    contenedor.appendChild(boton);
+    contenedorBody.appendChild(contenedorTodos);
+    contenedorBody.appendChild(contenedorRojo);
+    contenedorBody.appendChild(contenedorVerde);
+    contenedorBody.appendChild(contenedorAzul);
+
+    for (let i = 0; i < 20; i++) {
+
+        let contenedor = document.createElement("div");
+        contenedor.className = "gallery div";
+        contenedor.style.width = "100 px";
+        contenedor.style.height = "50 px";
+
+        let n = Math.floor(Math.random() * 3);
+
+        switch (n) {
+
+            case 0:
+
+                contenedor.style.backgroundColor = "blue";
+                contenedor.textContent = "Azul";
+
+                break;
+
+            case 1:
+
+                contenedor.style.backgroundColor = "green";
+                contenedor.textContent = "Verde";
+
+                break;
+
+            case 2:
+
+                contenedor.style.backgroundColor = "red";
+                contenedor.textContent = "Rojo";
+
+                break;
+        }
+
+        componentes.push(contenedor);
+        contenedorTodos.appendChild(contenedor);
+    }
 }
 
-contenedorPrincipal.appendChild(contenedor);
-cuerpo.appendChild(contenedorPrincipal);
+function mover() {
+
+    componentes.forEach(recorrer);
+
+    function recorrer(item, indice) {
+
+        if (item.textContent == "Azul") {
+
+            contenedorAzul.appendChild(item);
+
+        } else if (item.textContent == "Rojo") {
+
+            contenedorRojo.appendChild(item);
+
+        } else if (item.textContent == "Verde") {
+
+            contenedorVerde.appendChild(item);
+        }
+    }
+}
