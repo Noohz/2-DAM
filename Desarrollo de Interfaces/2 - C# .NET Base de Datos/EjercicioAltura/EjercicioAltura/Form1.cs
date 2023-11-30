5dc9284391550f4da93f05e5741a3d74a456af6d
Aitor Ramos <aitorramossanchez2001@gmail.com>
2023-11-30 20:29:28 +0100

    Add files via upload

---
+++ Desarrollo de Interfaces/2 - C# .NET Base de Datos/EjercicioAltura/EjercicioAltura/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Google.Protobuf.Collections;
+
+namespace EjercicioAltura
+{
+    public partial class Form1 : Form
+    {
+
+        ConectarBD cnx = new ConectarBD();
+        List<ClaseAltura> cAltura = new List<ClaseAltura>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            cAltura = cnx.DatosAltura();
+            dataGridView1.DataSource = cAltura;
+        }
+
+        private void btnIntroducirDatos_Click(object sender, System.EventArgs e)
+        {
+            cnx.insertarDatosAltura(textBoxProvincia.Text, textBoxSituacionAltMax.Text, Convert.ToInt16(textBoxAlturaMaxima.Text), textBoxSituacionAltMin.Text, Convert.ToInt16(textBoxAlturaMinima.Text));
+        }
+    }
+}
