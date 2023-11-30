67a43cfb68790a0d8af0d650572bfa8db47b1f2a
Aitor Ramos <aitorramossanchez2001@gmail.com>
2023-11-30 14:10:22 +0100

    Add files via upload

---
+++ Desarrollo de Interfaces/1 - Introducción a .NET con C#/Examen22Sept/ExamenEj2/ExamenEj2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ExamenEj2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.flp1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(205, 100);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(355, 100);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // flp1
+            // 
+            this.flp1.BackColor = System.Drawing.Color.Gold;
+            this.flp1.Location = new System.Drawing.Point(13, 127);
+            this.flp1.Name = "flp1";
+            this.flp1.Size = new System.Drawing.Size(775, 258);
+            this.flp1.TabIndex = 3;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(513, 99);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.flp1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.FlowLayoutPanel flp1;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Desarrollo de Interfaces/1 - Introducción a .NET con C#/Examen22Sept/ExamenEj2/ExamenEj2/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ExamenEj2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 6; i++)
+            {
+                comboBox2.Items.Add(i);
+            }
+
+            comboBox1.Items.Add("ComboBox");
+            comboBox1.Items.Add("TextBox");
+            comboBox1.Items.Add("Label");
+            comboBox1.Items.Add("Button");
+
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            flp1.Controls.Clear();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int nElementos = Convert.ToInt32(comboBox2.Text);
+
+            if (comboBox1.SelectedIndex == 0)
+            {
+                flp1.Controls.Clear();
+
+                for (int i = 0; i < nElementos; i++)
+                {
+                    ComboBox cb1 = new ComboBox();
+
+                    flp1.Controls.Add(cb1);
+                }
+
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+
+                flp1.Controls.Clear();
+
+                for (int i = 0; i < nElementos; i++)
+                {
+                    TextBox tb1 = new TextBox();
+
+                    flp1.Controls.Add(tb1);
+                }
+
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+
+                flp1.Controls.Clear();
+
+                for (int i = 0; i < nElementos; i++)
+                {
+                    Label lb1 = new Label();
+                    lb1.Text = "Label" + i;
+
+                    flp1.Controls.Add(lb1);
+                }
+
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+
+                flp1.Controls.Clear();
+
+                for (int i = 0; i < nElementos; i++)
+                {
+                    Button btn1 = new Button();
+                    btn1.BackColor = Color.Red;
+
+                    flp1.Controls.Add(btn1);
+                }
+
+            }
+        }
+    }
+}
