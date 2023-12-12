15250d83b98c76960dfc69ae2e687ef63c8a1684
Aitor Ramos <aitorramossanchez2001@gmail.com>
2023-12-12 13:30:33 +0100

    12/12/2023

--- Programación Multimedia/AndroidStudioProjects/Unidad2/TareaUD2/app/src/main/java/com/example/tareaud2/MainActivity.java
+++ Programación Multimedia/AndroidStudioProjects/Unidad2/TareaUD2/app/src/main/java/com/example/tareaud2/MainActivity.java
@@ -11,12 +11,12 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editTextTextUsuario, editTextTextContrasenia;
-    Button buttonIniciarSesion;
+    Button buttonIniciarSesion, buttonCrearUsuario;
+    AccesoDatos baseBD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         editTextTextUsuario = findViewById(R.id.editTextTextUsuario);
         editTextTextContrasenia = findViewById(R.id.editTextTextContrasenia);
         buttonIniciarSesion = findViewById(R.id.buttonIniciarSesion);
+        buttonCrearUsuario = findViewById(R.id.buttonCrearUsuario);
 
         buttonIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,9 +35,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        buttonCrearUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventoCrearUsuario();
+            }
+        });
+    }
+
+    private void EventoCrearUsuario() {
+        Intent mostrarCrearUsuario = new Intent(MainActivity.this, PantallaCrearUsuario.class);
+        startActivity(mostrarCrearUsuario);
     }
 
     private void EventoIniciarSesion() {
+        baseBD = new AccesoDatos(this);
 
         if (editTextTextUsuario.getText().toString().isEmpty() || editTextTextContrasenia.getText().toString().isEmpty()) {
             AlertDialog.Builder not1 = new AlertDialog.Builder(MainActivity.this);
@@ -50,9 +63,26 @@ public class MainActivity extends AppCompatActivity {
             });
             not1.show();
         } else {
-            Intent mostrarActividad2 = new Intent(MainActivity.this, Actividad2.class);
-            mostrarActividad2.putExtra("usuario", editTextTextUsuario.getText().toString());
-            startActivity(mostrarActividad2);
+            Usuario usuario = baseBD.buscarUsuario(editTextTextUsuario.getText().toString(), editTextTextContrasenia.getText().toString());
+
+            if (usuario != null) {
+                Intent mostrarActividad2 = new Intent(MainActivity.this, Actividad2.class);
+                mostrarActividad2.putExtra("usuario", usuario.getNombre());
+                mostrarActividad2.putExtra("contrasenia", usuario.getContrasenia());
+                mostrarActividad2.putExtra("email", usuario.getEmail());
+                startActivity(mostrarActividad2);
+            } else {
+                AlertDialog.Builder not2 = new AlertDialog.Builder(MainActivity.this);
+                not2.setTitle("Credenciales Incorrectas");
+                not2.setMessage("El nombre de usuario o la contraseña son incorrectos.");
+                not2.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                not2.show();
+            }
         }
         editTextTextUsuario.setText("");
         editTextTextContrasenia.setText("");
